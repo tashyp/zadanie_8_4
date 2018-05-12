a3de81da7cb3d17ef096e7976d9a627b7ff9360a
Przemek Saja <przemek.saja@gmail.com>
2018-05-12 21:58:48 +0200

    Add index.html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+// some comment
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+
+console.log('Hello, ' + name);
+/*
+console.log is the same as alert. Value will display at Console in DevTools
+*/
